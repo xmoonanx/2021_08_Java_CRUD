e09dfcae26c229526af7e9946a6877dce2ba7f04
xmoonanx <xmoonanx@gmail.com>
2021-08-20 18:53:36 +0900

    list project

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -6,19 +6,38 @@ import java.util.Scanner; // ctrl + shift + o
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램시작==");
-		
 		Scanner sc = new Scanner(System.in);
-		System.out.printf("명령어) ");
-		String command;
-		command = sc.nextLine().trim();
+		int lastArticleId = 0;
 		
-		System.out.printf("입력된 명령어 : %s\n", command);
+		while(true) {
+			System.out.printf("명령어) ");
+			String command = sc.nextLine().trim();
+			if(command.length()==0) {
+				continue;
+			}
+			
+			if(command.equals("system exit")) {
+				break;
+			}
+			if(command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.println("제목 : ");
+				String title = sc.nextLine();
+				System.out.println("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+			}
+			else if(command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
+			}
+		}
 		
-//		System.out.printf("명령어) ");
-//		int number = sc.nextInt();
-//		
-//		System.out.printf("입력된 숫자 : %d\n", number);
-
+		
 		
 		sc.close();
 		System.out.println("==프로그램끝==");
